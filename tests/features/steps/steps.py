8b193a50aa642e1758bf8c9855f21e54ba954056
Brian Snyder <bsnyder@technology4retailers.com>
2016-04-26 16:59:43 -0500

    working on test for addressbook already exists

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -2,9 +2,16 @@ from behave import given, when, then
 #import addressbook
 import mock
 
+
 @when('user creates an addressbook called "{addressbook_name}"')
 def create_addressbook(context, addressbook_name):
     context.addressbook.name = addressbook_name
+
+
+@when('"{addressbook_name}" already exists in the database')
+def check_if_addressbook_alerady_exits(context, addressbook_name):
+    context.addressbook.find_name = lambda: addressbook_name
+
 
 @then('a new addressbook named "{addressbook_name}" should be created')
 def check_if_new_addressbook_created(context, addressbook_name):
